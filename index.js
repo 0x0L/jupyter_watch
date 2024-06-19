eec13e3196f9ade49abac5e83d912d4670b70356
nullptr <3621629+0x0L@users.noreply.github.com>
2024-06-19 22:15:22 +0200

    errors and more

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-var ws = new WebSocket("ws://127.0.0.1:8765");
 function log(type, content) {
+  // https://jupyter-client.readthedocs.io/en/stable/messaging.html#messages-on-the-iopub-pub-sub-channel
+  host = document.getElementById("console");
   if (type == "execute_input") {
-    var node = document.createElement("div");
-    node.textContent = "> " + content["code"];
+    var node = document.createElement("pre");
+    node.innerText = content["code"];
     node.className = type;
-    document.getElementById("console").appendChild(node);
+    host.appendChild(node);
+  } else if (type == 'error') {
+    var node = document.createElement("pre");
+    node.innerText = content['ename'] + ': ' + content["evalue"];
+    node.className = type;
+    host.appendChild(node);
   } else if (type == "stream") {
-    var node = document.createElement("div");
-    node.textContent = content["text"];
+    var node = document.createElement("pre");
+    node.innerText = content["text"];
     node.className = type;
-    document.getElementById("console").appendChild(node);
+    host.appendChild(node);
   } else if (type == "execute_result" || type == "display_data") {
     Object.keys(content["data"]).forEach(function (key) {
       if (key == "text/plain") {
-        var node = document.createElement("div");
-        node.textContent = content["data"][key];
+        var node = document.createElement("pre");
+        node.innerText = content["data"][key];
         node.className = type;
-        document.getElementById("console").appendChild(node);
-      } else if (key == "image/png") {
+        host.appendChild(node);
+      } else if (key == "text/html") {
+        var node = document.createElement("div");
+        node.innerHTML = content["data"][key];
+        node.className = type;
+        host.appendChild(node);
+      } else if ((key == "image/png") || (key == 'image/svg+xml')) {
         src = "data:" + key + ";base64," + content["data"][key];
         var node = document.createElement("img");
         node.src = src;
-        document.getElementById("console").appendChild(node);
+        host.appendChild(node);
       }
       //console.log(key, content["data"][key]);
     });
   }
   window.scrollTo(0, document.body.scrollHeight);
-  // else {"msg_type": "status", "content": {"execution_state": "idle"}} / busy
-  // {"msg_type": "error", "content": {"traceback": ["\u001b[0;31m..."], "ename": "NameError", "evalue": "name 'z' is not defined"}}
-  // https://jupyter-client.readthedocs.io/en/stable/messaging.html#messages-on-the-iopub-pub-sub-channel
 }
+
+var ws = new WebSocket("ws://127.0.0.1:8765");
 ws.onmessage = function (ev) {
   msg = JSON.parse(ev["data"]);
   log(msg["msg_type"], msg["content"]);
